3514febf05b6aa88e9feb90cf0d97db5b2f2cd79
Vladimir Sorokin <sorokinv@hotmail.com>
2018-02-12 06:50:11 +1000

    V02+

--- Python/LinkNetBoba.py
+++ Python/LinkNetBoba.py
@@ -28,6 +28,10 @@
 #            3. Add activation parameter for last layer
 #            4. Add conv2D before last layer
 #
+# 2018-02-11
+#            1. Fix bugs (acti_last --> acti in pre-last activation)
+#            2. Add prefix-name parameter
+#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 from keras.models import Input, Model
@@ -35,7 +39,10 @@ from keras.layers import Convolution2D, MaxPooling2D, UpSampling2D, Conv2D, Conc
 from keras.layers import Conv2DTranspose, SpatialDropout2D
 from keras.layers.normalization import BatchNormalization
 
-def LinkNetBoba (img_shape, n_out=1, depth=4, acti='elu', dropout=False, batch=True, acti_last='sigmoid', printOK=False):
+def LinkNetBoba (img_shape, n_out=1, depth=4, 
+                 acti='elu', acti_last='sigmoid', 
+                 dropout=False, batch=True, 
+                 prefix='boba_',printOK=False):
     
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Encoder block
@@ -47,28 +54,28 @@ def LinkNetBoba (img_shape, n_out=1, depth=4, acti='elu', dropout=False, batch=T
         
         #print('e 0 depth=',depth,io.shape)
         
-        io = Conv2D(nn, (3, 3), strides=2, padding='same', name='conv1d'+str(maxDepth-depth))(io)
-        if batch : io = BatchNormalization(name='bath1d'+str(maxDepth-depth))(io)
+        io = Conv2D(nn, (3, 3), strides=2, padding='same', name=prefix+'conv1d'+str(maxDepth-depth))(io)
+        if batch : io = BatchNormalization(name=prefix+'bath1d'+str(maxDepth-depth))(io)
         io = Activation(acti)(io)
         
         #print('e 1 depth=',depth,io.shape)
         
-        io = Conv2D(nn, (3, 3), padding='same', name='conv2d'+str(maxDepth-depth))(io)
-        if batch : io = BatchNormalization(name='bath2d'+str(maxDepth-depth))(io)
+        io = Conv2D(nn, (3, 3), padding='same', name=prefix+'conv2d'+str(maxDepth-depth))(io)
+        if batch : io = BatchNormalization(name=prefix+'bath2d'+str(maxDepth-depth))(io)
         
-        ii = Conv2D(nn, (1, 1), strides=2, name='ii1d'+str(maxDepth-depth))(i)
-        if batch : io = BatchNormalization(name='bath3d'+str(maxDepth-depth))(io) # 2017-12-11 add batch
+        ii = Conv2D(nn, (1, 1), strides=2, name=prefix+'ii1d'+str(maxDepth-depth))(i)
+        if batch : io = BatchNormalization(name=prefix+'bath3d'+str(maxDepth-depth))(io) # 2017-12-11 add batch
         
         io = Add()([io, ii]); io1 = io;
         
         io = Activation(acti)(io)   # 2017-12-11 change point add before activation after batch
         
-        io = Conv2D(nn, (3, 3), padding='same', name='conv3d'+str(maxDepth-depth))(io)
-        if batch : io = BatchNormalization(name='bath4d'+str(maxDepth-depth))(io)
+        io = Conv2D(nn, (3, 3), padding='same', name=prefix+'conv3d'+str(maxDepth-depth))(io)
+        if batch : io = BatchNormalization(name=prefix+'bath4d'+str(maxDepth-depth))(io)
         io = Activation(acti)(io)                
         
-        io = Conv2D(nn, (3, 3), padding='same', name='conv4d'+str(maxDepth-depth))(io)
-        if batch : io = BatchNormalization(name='bath5d'+str(maxDepth-depth))(io)
+        io = Conv2D(nn, (3, 3), padding='same', name=prefix+'conv4d'+str(maxDepth-depth))(io)
+        if batch : io = BatchNormalization(name=prefix+'bath5d'+str(maxDepth-depth))(io)
         
         io = Add()([io, io1]);     # 2017-12-11 change point add before activation after batch
         
@@ -87,18 +94,18 @@ def LinkNetBoba (img_shape, n_out=1, depth=4, acti='elu', dropout=False, batch=T
         
         io = i
         
-        io = Conv2D(mm//4, (1, 1), padding='same', name='convd1d'+str(maxDepth-depth))(io)
-        if batch : io = BatchNormalization(name='bathd1d'+str(maxDepth-depth))(io)
+        io = Conv2D(mm//4, (1, 1), padding='same', name=prefix+'convd1d'+str(maxDepth-depth))(io)
+        if batch : io = BatchNormalization(name=prefix+'bathd1d'+str(maxDepth-depth))(io)
         io = Activation(acti)(io)                
         
-        io = Conv2DTranspose(mm//4, (3, 3), padding='same', name='convd2d'+str(maxDepth-depth))(io)
-        if batch : io = BatchNormalization(name='bathd2d'+str(maxDepth-depth))(io)
+        io = Conv2DTranspose(mm//4, (3, 3), padding='same', name=prefix+'convd2d'+str(maxDepth-depth))(io)
+        if batch : io = BatchNormalization(name=prefix+'bathd2d'+str(maxDepth-depth))(io)
         io = Activation(acti)(io)    
         
         io = UpSampling2D((2,2))(io)
         
-        io = Conv2D(nn, (1, 1), padding='same', name='convd3d'+str(maxDepth-depth))(io)
-        if batch : io = BatchNormalization(name='bathd3d'+str(maxDepth-depth))(io)
+        io = Conv2D(nn, (1, 1), padding='same', name=prefix+'convd3d'+str(maxDepth-depth))(io)
+        if batch : io = BatchNormalization(name=prefix+'bathd3d'+str(maxDepth-depth))(io)
         io = Activation(acti)(io)                
         
         return (io)
@@ -150,9 +157,9 @@ def LinkNetBoba (img_shape, n_out=1, depth=4, acti='elu', dropout=False, batch=T
     dmm0 = enn0
     dnn0 = emm0
     
-    i = Input(shape=img_shape, name='input'); io = i
+    i = Input(shape=img_shape, name=prefix+'input'); io = i
     if True : # always exists  
-        io = BatchNormalization(name='bath0d'+str(maxDepth-depth))(io)
+        io = BatchNormalization(name=prefix+'bath0d'+str(maxDepth-depth))(io)
     
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Initial function block
@@ -160,11 +167,11 @@ def LinkNetBoba (img_shape, n_out=1, depth=4, acti='elu', dropout=False, batch=T
     
     #print(io.shape)
 
-    io = Conv2D(64, (7, 7), strides=2, padding='same', name='conv1d'+str(maxDepth-depth))(io)
-    if batch : io = BatchNormalization(name='bath1d'+str(maxDepth-depth))(io)
+    io = Conv2D(64, (7, 7), strides=2, padding='same', name=prefix+'conv1d'+str(maxDepth-depth))(io)
+    if batch : io = BatchNormalization(name=prefix+'bath1d'+str(maxDepth-depth))(io)
     io = Activation(acti)(io)                
         
-    io = MaxPooling2D((3, 3), strides=2, name='pool1d'+str(maxDepth-depth))(io)
+    io = MaxPooling2D((3, 3), strides=2, name=prefix+'pool1d'+str(maxDepth-depth))(io)
     
     ##print('before',io.shape)
     
@@ -180,17 +187,17 @@ def LinkNetBoba (img_shape, n_out=1, depth=4, acti='elu', dropout=False, batch=T
     # Final function block
     #
 
-    io = Conv2DTranspose(32, (3, 3), padding='same', name='conv2d'+str(maxDepth-depth))(io)
-    if batch : io = BatchNormalization(name='bath2d'+str(maxDepth-depth))(io)
+    io = Conv2DTranspose(32, (3, 3), padding='same', name=prefix+'conv2d'+str(maxDepth-depth))(io)
+    if batch : io = BatchNormalization(name=prefix+'bath2d'+str(maxDepth-depth))(io)
     io = Activation(acti)(io)                
     
     io = UpSampling2D((2,2))(io)
         
-    io = Conv2D(32, (3,3), padding='same', name='conv3d'+str(maxDepth-depth))(io)
-    if batch : io = BatchNormalization(name='bath3d'+str(maxDepth-depth))(io)
+    io = Conv2D(32, (3,3), padding='same', name=prefix+'conv3d'+str(maxDepth-depth))(io)
+    if batch : io = BatchNormalization(name=prefix+'bath3d'+str(maxDepth-depth))(io)
     io = Activation(acti)(io)                
     
-    if dropout : io = SpatialDropout2D(rate=dropout, name='dropLd'+str(maxDepth-depth))(io)
+    if dropout : io = SpatialDropout2D(rate=dropout, name=prefix+'dropLd'+str(maxDepth-depth))(io)
         
     ###io = UpSampling2D((2,2))(io) -- ?????? do bad output result 
     
@@ -198,15 +205,15 @@ def LinkNetBoba (img_shape, n_out=1, depth=4, acti='elu', dropout=False, batch=T
     # The End. Build output
     #
     
-    io = Conv2DTranspose(n_out, (2, 2), strides=2, padding='same', name='conv4d'+str(maxDepth-depth))(io)
-    if batch : io = BatchNormalization(name='bath4d'+str(maxDepth-depth))(io)
+    io = Conv2DTranspose(n_out, (2, 2), strides=2, padding='same', name=prefix+'conv4d'+str(maxDepth-depth))(io)
+    if batch : io = BatchNormalization(name=prefix+'bath4d'+str(maxDepth-depth))(io)
     io = Activation(acti)(io)                
 
-    io = Conv2D(n_out, (3,3), padding='same', name='conv5d'+str(maxDepth-depth))(io)
-    if batch : io = BatchNormalization(name='bath5d'+str(maxDepth-depth))(io)
-    io = Activation(acti_last)(io)
+    io = Conv2D(n_out, (3,3), padding='same', name=prefix+'conv5d'+str(maxDepth-depth))(io)
+    if batch : io = BatchNormalization(name=prefix+'bath5d'+str(maxDepth-depth))(io)
+    io = Activation(acti)(io)
 
-    io = Conv2D(n_out, (1,1), padding='same', name='convLd'+str(maxDepth-depth))(io)
+    io = Conv2D(n_out, (1,1), padding='same', name=prefix+'convLd'+str(maxDepth-depth))(io)
     o = Activation(acti_last)(io)
 
     return Model(inputs=i, outputs=o, name='LinkNetBoba')
